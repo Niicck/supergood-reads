f5d63340733688ba0e63a440377fe6252b6fa5e1
Niicck <niicck@users.noreply.github.com>
2023-09-21 15:58:20 -0500

    remove old sample_app

--- tests/sample_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from supergood_reads.admin import ReviewAdmin, UserSettingsAdmin
-from supergood_reads.models import Review, UserSettings
-
-admin.site.register(Review, ReviewAdmin)
-admin.site.register(UserSettings, UserSettingsAdmin)
